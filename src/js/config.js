b61ef289e9978bf6104240cafdf5b8e93d6777a4
Qetuose <lukasbaltrimas123@gmail.com>
2024-08-11 16:49:20 +0300

    Implementing upload

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
+export const KEY = '91638464-d20c-4ba2-b66c-2a213e0587d2';
+export const MODAL_CLOSE_SEC = 2.5;
